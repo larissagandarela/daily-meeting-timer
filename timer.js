eb79b8a3df541ad06a0b65e5b683759dcbbccc25
mustache1up <daniel.sp@msn.com>
2020-05-10 18:36:41 -0300

    possibilita insercao de mais contadores

--- timer.js
+++ timer.js
@@ -1,11 +1,12 @@
 
 // atribuicoes inicias
 var todosTimersParados = true;
+var indiceDoContador = 0;
 
-function alternaEstadoContagem() {
+function alternaEstadoContagem(indice) {
       
     if(todosTimersParados) {
-        iniciarContagem();
+        iniciarContagem(indice);
         //alterar texto do botao para "Parar"
     } else {
         pararContagem();
@@ -18,13 +19,13 @@ function pararContagem() {
     todosTimersParados = true;
 }
 
-async function iniciarContagem() {
+async function iniciarContagem(indice) {
 	
     todosTimersParados = false;
     
     while(!todosTimersParados)	{
 		
-		var cont = document.getElementById("contador");
+		var cont = document.getElementById("contador-" + indice);
 		var novoValor = cont.value - 1;
 		cont.value = novoValor;
 		await sleep(1000);
@@ -35,3 +36,46 @@ function sleep(milissegundos) {
 	
     return new Promise(resolve => setTimeout(resolve, milissegundos));
 }
+
+function adicionarContador() {
+    
+    indiceDoContador = indiceDoContador + 1;
+    
+    //pega o template e o container
+    var template  = document.getElementById("timer-template");
+    var container = document.getElementById("timer-container")
+    
+    //duplica o template
+    var clone = template.cloneNode(true);
+    
+    var contadoresDentroDoClone = clone.getElementsByClassName("contador");
+    var contadorDentroDoClone = contadoresDentroDoClone[0];
+    contadorDentroDoClone.id = "contador-" + indiceDoContador;
+    
+    var botoesDentroDoClone = clone.getElementsByClassName("btn_play_pause");
+    var botaoDentroDoClone = botoesDentroDoClone[0];
+    botaoDentroDoClone.setAttribute("onClick", "alternaEstadoContagem(" + indiceDoContador + ")");
+    
+    container.appendChild(clone);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
